c0974ebc5a0d033a7986a1d066bcb4edcf8e30f8
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-02-03 13:31:34 -0600

    Z-score analysis is now functioning. Created script for creating z-scores for the team data.

--- Team Data Analysis.R
+++ Team Data Analysis.R
@@ -1,14 +1,6 @@
-indData <- indData %>%                               # modify current data
+teamData <- teamData %>%                             # modify current data
   group_by(position, game_year) %>%                  # group by player and year
   mutate_at(vars(-name, -position, -game_year),      # unselect character vars
                                                      # and year var
             funs(z = ((. - mean(.)) / sd(.))))       # create z-score for all 
-                                                     # variables
-
-
-
-#zscore <- function(x) {
-#  z <- ((x - paste0(x, "", "_average", 
-#                    collapse = NULL)) / paste(x, "", "_stdv", collapse = NULL))
-#  return(z)
-#}
+                                                     # variables 
