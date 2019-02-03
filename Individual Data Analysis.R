fc5fd8e8bf0130e9a22f8219f20393822139a2c6
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2019-02-03 15:03:44 -0600

    Both scripts have been debugged and a fully functional

--- Individual Data Analysis.R
+++ Individual Data Analysis.R
@@ -6,46 +6,46 @@ indData <- indData %>%                               # modify current data
                                                      # variables
 
   ungroup() %>%                                      # ungroup
-  select(name, position, contains("z")) %>%          # restrict vars to zscores
-  group_by(name, position) %>%                       # group by name and position
+  select(name, position, game_year, contains("z"))%>%# restrict vars to zscores
+  group_by(name, position, game_year) %>%            # group by name and position
   summarise_at(vars(-name, -position), funs(sumZ = sum)) %>% #sum zscores across years
-  select(name, position, contains("sumZ"))           # restrict vars to zscore sums
+  select(name, game_year, position, contains("sumZ"))# restrict vars to zscore sums
 
 rb <- indData %>%                                    # create dataset for RBs
+  select(name, game_year, position, contains("rush")) %>% # select rushing vars
   filter(position == "RB") %>%                       # filter to RB position
-  select(name, contains("rush")) %>%                 # select rushing vars
   mutate(rush_fumbles_z_sumZ = rush_fumbles_z_sumZ * -1) %>% # reverse score fumbles
-  group_by(name) %>%                                 # group by player name
+  ungroup() %>%
+  group_by(name, game_year) %>%                      # group by player name
   summarise(totalZ = rush_att_z_sumZ + rush_yds_z_sumZ + rush_avg_z_sumZ + 
               rush_tds_z_sumZ + rush_fumbles_z_sumZ) %>%   # create total z score index
   arrange(desc(totalZ))                              # arrange with greatest valuea at top
 
 qb <- indData %>%                                    # create dataset for QBs
+  select(name, position, game_year, contains("pass"), contains("int"), 
+         contains("sck"), contains("rate")) %>%      # select passing vars
   filter(position == "QB") %>%                       # filter down to QB position
-  select(name, contains("pass"), contains("int"), contains("sck"), 
-         contains("rate")) %>%                       # select passing vars
   mutate(int_z_sumZ = int_z_sumZ * -1,
          sck_z_sumZ = sck_z_sumZ * -1,
          pass_fumbles_z_sumZ = pass_fumbles_z_sumZ * -1) %>%   # reverse score ints, scks, and fumbles
-  group_by(name) %>%                                 # group by player name
+  ungroup() %>%
+  group_by(name, game_year) %>%                      # group by player name
   summarise(totalZ = pass_att_z_sumZ + pass_yds_z_sumZ + pass_tds_z_sumZ +
               int_z_sumZ + sck_z_sumZ + pass_fumbles_z_sumZ + rate_z_sumZ) %>%  # create total z score index
   arrange(desc(totalZ))                              # arrange with greatest valuea at top
 
 wr_te <- indData %>%                                 # create dataset for WRs and TEs
+  select(name, position, game_year, contains("rec")) %>% # select receiving vars
   filter(position == "WR/TE") %>%                    # filter down to wr/te
-  select(name, contains("rec")) %>%                  # select receiving vars
-  group_by(name) %>%                                 # group by player name
+  ungroup() %>%
+  group_by(name, game_year) %>%                      # group by player name
   mutate(rec_fumbles_z_sumZ = rec_fumbles_z_sumZ * -1) %>%  # reverse score fumbles
   summarise(totalZ = rec_z_sumZ + rec_yds_z_sumZ + rec_avg_z_sumZ + 
               rec_tds_z_sumZ + rec_fumbles_z_sumZ) %>% # create total z score index
   arrange(desc(totalZ))                              # arrange with greatest valuea at top
 
-rb <- as.tibble(rb)                                  # convert rb to a tibble
 rb                                                   # print rb
 
-qb <- as.tibble(qb)                                  # convert qb to a tibble
 qb                                                   # print qb
 
-wr_te <- as.tibble(wr_te)                            # convert wr_te to a tibble
 wr_te                                                # print wr_te
--- Team Data Analysis.R
+++ Team Data Analysis.R
@@ -6,14 +6,16 @@ teamData <- teamData %>%                             # modify current data
                                                      # variables 
 
 ungroup() %>%                                        # ungroup
-  select(team, contains("z")) %>%                    # restrict vars to zscores
-  group_by(team) %>%                                 # group by name and position
-  summarise_at(vars(-team), funs(sumZ = sum(., na.rm = TRUE))) %>%    #sum zscores across years
+  select(team, game_year, contains("z")) %>%         # restrict vars to zscores
+  group_by(team, game_year) %>%                      # group by name and position
+  summarise_at(vars(-team, -game_year), funs(sumZ = sum(., na.rm = TRUE))) %>%    #sum zscores across years
   mutate(rush_fumbles_z_sumZ = rush_fumbles_z_sumZ * -1,
          rec_fumbles_z_sumZ = rec_fumbles_z_sumZ * -1,
          int_z_sumZ = int_z_sumZ * -1,
          sck_z_sumZ = sck_z_sumZ * -1,
          pass_fumbles_z_sumZ = pass_fumbles_z_sumZ * -1) %>% # reverse score fumbles, sacks, and ints
+  ungroup() %>%
+  group_by(team, game_year) %>%
   summarise(totalZ = rush_att_z_sumZ + rush_yds_z_sumZ + rush_avg_z_sumZ + 
               rush_tds_z_sumZ + rush_fumbles_z_sumZ + rec_z_sumZ + 
               rec_yds_z_sumZ + rec_avg_z_sumZ + rec_tds_z_sumZ +
@@ -21,3 +23,5 @@ ungroup() %>%                                        # ungroup
               pass_tds_z_sumZ + int_z_sumZ + sck_z_sumZ + 
               pass_fumbles_z_sumZ) %>%               # create total z score index
   arrange(desc(totalZ))                              # sort in descending order
+
+teamData                                             # print teamData
